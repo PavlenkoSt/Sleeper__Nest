ff7a33f83e8e8e248cede7443a9a44484b1347b9
PavlenkoSt <verbatim8872@gmail.com>
2025-09-08 20:52:49 +0300

    refactoring - remove config lib, setup .env for each microservice

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,6 +4,8 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { Reservation, ReservationSchema } from './entities/reservation.entity';
 import { ReservationsRepository } from './reservations.repository';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
@@ -12,6 +14,13 @@ import { ReservationsRepository } from './reservations.repository';
       { name: Reservation.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
